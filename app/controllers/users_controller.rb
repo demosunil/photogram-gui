650fc519c5e521da4ce5ca46117d5c17d81a6544
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-03-08 21:49:48 +0000

    User details looks good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ matching_users = User.all
   render({:template =>"user_templates/index.html.erb"})
 
 end
-end
+def show
+# parameters: {"path_username" => "anisa"}
+url_username =params.fetch("path_username")
+matching_usernames = User.where({:username=> url_username})
+@the_user = matching_usernames.at(0)
+  #if @the_user == nil
+   # redirect_to("/user",status:404)
+   # or redirect_to("/404") we have 404.html inside public folder page here
+  #else
+    render({:template =>"user_templates/show.html.erb"})
+    #end
+  end
+  end
